a5d7c1c244210cfd09cc51f07dddf07233a19980
Duarte Santos <duarte.n.santos@gmail.com>
2025-12-30 17:22:58 +0000

    chore: initial Laravel setup with Task model, enum and migration

---
+++ app/Models/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\TaskStatus;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'title',
+        'status',
+    ];
+
+    protected $casts = [
+        'status' => TaskStatus::class,
+    ];
+}
